28979d3728a3c0aaf807c58b8d3d0f7bb76d589b
fs <fs@imagine-orange.com>
2018-07-08 18:26:11 +0200

    Seed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormlyFieldConfig} from '@ngx-formly/core';
+import {Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./_app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    person = {
+        firstname: 'Juri',
+        age: 32,
+        gender: 'M'
+    };
+
+    personFields = <FormlyFieldConfig>[
+        {
+            key: 'firstname',
+            type: 'input',
+            templateOptions: {
+                label: 'Firstname'
+            },
+            validators: {
+                validation: Validators.required
+            },
+            validation: {
+                messages: {
+                    required: 'You need to provide a value'
+                }
+            }
+        },
+        {
+            key: 'age',
+            type: 'input',
+            templateOptions: {
+                label: 'Age',
+                type: 'number'
+            },
+            validators: {
+                validation: Validators.min(18)
+            },
+            validation: {
+                messages: {
+                    min: 'You need to specify a value greater or equal to 18'
+                }
+            }
+        },
+        {
+            key: 'gender',
+            type: 'radio',
+            templateOptions: {
+                label: 'Gender',
+                options: [{value: 'Male', key: 'M'}, {value: 'Female', key: 'F'}]
+            }
+        }
+    ];
+
+    onAction() {
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './@material/material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormlyModule} from '@ngx-formly/core';
+import {FormlyMaterialModule} from '@ngx-formly/material';
+import {DynamicFormComponent} from './dynamic-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DynamicFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        FormlyModule,
+        ReactiveFormsModule,
+        FormlyModule.forRoot(),
+        FormlyMaterialModule
+
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
